3a49b36424ce9e627d7a9522f98d27186ef2f6c1
Rémy Bach <remy@bach.me.uk>
2012-03-15 13:32:36 +0000

    Doh! Actually check whether labelLeft or labelTop are NOT numbers before trying to pull out the numbers.

--- jquery.superLabels.js
+++ jquery.superLabels.js
@@ -30,9 +30,11 @@
 		// If this has been run on an empty set of elements, pop out.
 		if (this.length === 0) return false;
 
-		// Remove any NaNs from the left and top positions (so you could pass in 5px for example)
-		options.labelLeft = Number(options.labelLeft.replace(/\D+/, ''));
-		options.labelTop = Number(options.labelTop.replace(/\D+/, ''));
+		// Remove any NaNs from the positions (if present)
+		if (isNaN(options.labelLeft))
+			options.labelLeft = Number(options.labelLeft.replace(/\D+/, ''));
+		if (isNaN(options.labelTop))
+			options.labelTop = Number(options.labelTop.replace(/\D+/, ''));
 		
 		// If options were passed in, merge them with the defaults.
 		$.extend(defaults, options || {});
