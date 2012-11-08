7dbb819386487d911eb19a3d716ff8e93bb330b1
Remy Bach <remy@bach.me.uk>
2012-11-08 21:52:00 +0000

    Upgrade the version number to 2.0 now that we have a decent demo, new features, and bug fixes.

--- jquery.superLabels.js
+++ jquery.superLabels.js
@@ -1,7 +1,7 @@
 /*
  *	Title: jQuery Super Labels Plugin - Give your forms a helping of awesome!
  *	Author: Rémy Bach
- *	Version: 1.2.2
+ *	Version: 2.0
  *	License: http://remybach.mit-license.org
  *	Url: http://github.com/remybach/jQuery.superLabels
  *	Description:
